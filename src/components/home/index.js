8fa1c4444710a2fd57581cc5eb4993f2712a0846
JP Barbosa <jp@jp7.com.br>
2017-05-26 18:05:35 -0300

    Changes to vehicles-dealer-prerendering

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -5,7 +5,17 @@ class HomeIndex extends Component {
   render() {
     return (
       <div className="home">
-        <div className="carousel">Home Carrossel</div>
+        <div className="carousel">
+          <h3>Home Carrossel Placeholder</h3>
+          <br />
+          <h4>This project is intented to test prerendering with Netlify</h4>
+          <br />
+          <a href="https://github.com/jp7jp/vehicles-dealer-prerendering">GitHub Repo</a>
+          <br /><br />
+          <a href="https://www.netlify.com">
+            <img src="https://www.netlify.com/img/global/badges/netlify-color-accent.svg"/>
+          </a>
+        </div>
         <div className="buttons">
           <div className="d-flex flex-row justify-content-center">
             <div><Link to="./" className="nav-link">Home</Link></div>
